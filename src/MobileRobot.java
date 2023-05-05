637661464fef2f61788ea4c209c9e3199e7ecf88
AnhelinaKyrylova <xkyrylova@stuba.sk>
2023-05-05 21:21:23 +0200

    1 version

--- src/MobileRobot.java
+++ src/MobileRobot.java
@@ -1,14 +1,71 @@
-import javax.swing.*;
+public class MobileRobot {
+    // Parameters
+    private static final double WHEEL_RADIUS = 0.05; // m
+    private static final double WHEEL_DISTANCE = 0.2; // m
+    private static final double WHEEL_CIRCUMFERENCE = 2 * Math.PI * WHEEL_RADIUS;
 
-public class RobotTrajectory extends JPanel {
+    public static void main(String[] args) {
+        // Input vectors
+        double[] time = {0, 5, 10, 15, 20}; // s
+        double[] leftWheelSpeeds = {2, -1, 0, 2, 1}; // m/s
+        double[] rightWheelSpeeds = {2, 1, 0, -2, 1}; // m/s
 
-    // vstupne udaje
-    private static final double L = 200.0; // rozchod kolies [mm]
-    private static final double r = 50.0; // polomer kolesa [mm]
-    private static final double d = 100.0; // velkost stvorca [mm]
+        // Calculate trajectories
+        double[][] wheelTrajectory = calculateWheelTrajectory(time, leftWheelSpeeds, rightWheelSpeeds);
+        double[][] centerOfMassTrajectory = calculateCenterOfMassTrajectory(time, leftWheelSpeeds, rightWheelSpeeds);
 
-    // vypocitane konstanty
-    private static final double v = d / 4.0; // rychlost robota [mm/s]
-    private static final double omega = Math.PI / 2.0 * v / r; // uhlova rychlost otacania robota [rad/s]
-    private static final double t_total = 16.0 * d / v / r;
+        // Print trajectories
+        System.out.println("Wheel Trajectory:");
+        printMatrix(wheelTrajectory);
+        System.out.println("Center of Mass Trajectory:");
+        printMatrix(centerOfMassTrajectory);
+    }
+
+    private static double[][] calculateWheelTrajectory(double[] time, double[] leftWheelSpeeds, double[] rightWheelSpeeds) {
+        double[][] wheelTrajectory = new double[2][time.length];
+        double leftWheelDistance = 0;
+        double rightWheelDistance = 0;
+        for (int i = 0; i < time.length; i++) {
+            double deltaLeftWheelDistance = leftWheelSpeeds[i] * WHEEL_CIRCUMFERENCE * (time[i] - (i > 0 ? time[i-1] : 0));
+            double deltaRightWheelDistance = rightWheelSpeeds[i] * WHEEL_CIRCUMFERENCE * (time[i] - (i > 0 ? time[i-1] : 0));
+            leftWheelDistance += deltaLeftWheelDistance;
+            rightWheelDistance += deltaRightWheelDistance;
+            double x = (leftWheelDistance + rightWheelDistance) / 2;
+            double y = (rightWheelDistance - leftWheelDistance) / WHEEL_DISTANCE;
+            wheelTrajectory[0][i] = x;
+            wheelTrajectory[1][i] = y;
+        }
+        return wheelTrajectory;
+    }
+
+    private static double[][] calculateCenterOfMassTrajectory(double[] time, double[] leftWheelSpeeds, double[] rightWheelSpeeds) {
+        double[][] centerOfMassTrajectory = new double[2][time.length];
+        double x = 0;
+        double y = 0;
+        double orientation = 0;
+        for (int i = 0; i < time.length; i++) {
+            double v = (leftWheelSpeeds[i] + rightWheelSpeeds[i]) / 2;
+            double w = (rightWheelSpeeds[i] - leftWheelSpeeds[i]) / WHEEL_DISTANCE;
+            double deltaOrientation = w * (time[i] - (i > 0 ? time[i-1] : 0));
+            orientation += deltaOrientation;
+            x += v * Math.cos(orientation) * (time[i] - (i > 0 ? time[i-1] : 0));
+            y += v * Math.sin(orientation) * (time[i] - (i > 0 ? time[i-1] : 0));
+            centerOfMassTrajectory[0][i] = x;
+            centerOfMassTrajectory[1][i] = y;
+        }
+        return centerOfMassTrajectory;
+    }
+
+    private static void printMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.printf("%.4f ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
+//Tento kód vypočíta trajektóriu kolies a trajektóriu ťažiska
+//pre zadané vektory času a rýchlostí ľavého a pravého kolesa.
+//Tieto trajektórie sú uložené v matici súradníc a môžu byť ďalej vizualizované
+// pomocou externých knižníc na vykresľovanie grafov.
